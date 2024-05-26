882572e0d28b18c87a9e4b21015c84924236dbb9
mazhar <swopnil420@gmail.com>
2024-05-26 09:08:26 +0600

    We created Create Prompt form, route, modal, and api. The prompt is creating successfully.

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,12 +2,39 @@
 
 import Form from '@components/Form';
 import React, { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
-const CratePrompt = () => {
+const CreatePrompt = () => {
+	const router = useRouter();
+	const { data: session } = useSession();
+
 	const [submitting, setSubmitting] = useState(false);
 	const [post, setPost] = useState({ prompt: '', tag: '' });
 
-	const createPrompt = async (e) => {};
+	const createPrompt = async (e) => {
+		e.preventDefault();
+		setSubmitting(true);
+
+		try {
+			const response = await fetch('/api/prompt/new', {
+				method: 'POST',
+				body: JSON.stringify({
+					prompt: post.prompt,
+					userId: session?.user.id,
+					tag: post.tag,
+				}),
+			});
+
+			if (response.ok) {
+				router.push('/');
+			}
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setSubmitting(false);
+		}
+	};
 
 	return (
 		<Form
@@ -20,4 +47,4 @@ const CratePrompt = () => {
 	);
 };
 
-export default CratePrompt;
+export default CreatePrompt;
