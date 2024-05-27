d99155f191508eb0db64ed414a24b0b84475cf61
mazhar <swopnil420@gmail.com>
2024-05-27 09:10:41 +0600

    implemented the edit and delete feature of prompt.

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,9 +2,11 @@
 
 import Profile from '@components/Profile';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 const UserProfile = () => {
+	const router = useRouter();
 	const { data: session } = useSession();
 
 	const [posts, setPosts] = useState([]);
@@ -23,9 +25,31 @@ const UserProfile = () => {
 		if (session?.user.id) fetchPosts();
 	}, []);
 
-	const handleEdit = () => {};
+	const handleEdit = (post) => {
+		router.push(`/update-prompt?id=${post._id}`);
+	};
 
-	const handleDelete = () => {};
+	const handleDelete = async (post) => {
+		const hasConfirmed = confirm(
+			'Are you sure you want to delete this prompt?'
+		);
+
+		if (hasConfirmed) {
+			try {
+				await fetch(`/api/prompt/${post._id.toString()}`, {
+					method: 'DELETE',
+				});
+
+				const filteredPosts = posts.filter(
+					(item) => item._id !== post._id
+				);
+
+				setPosts(filteredPosts);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	};
 
 	return (
 		<Profile
