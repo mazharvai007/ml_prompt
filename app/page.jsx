5155bd2b53dcd98aaddbd7a5f98efb7f5f43ac65
mazhar <swopnil420@gmail.com>
2024-05-29 22:57:01 +0600

    changed text

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 				</span>
 			</h1>
 			<p className="desc text-center">
-				Promptopia is an open-source AI prompting tool for modern world
+				PromptSave is an open-source AI prompting tool for modern world
 				to discover, create, and share creative prompts
 			</p>
 
