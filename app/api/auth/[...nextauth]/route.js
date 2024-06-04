c78c869bbca29bff4ee3664c3f01f89c17b3ab0a
mazhar <swopnil420@gmail.com>
2024-06-04 23:49:42 +0600

    added sessoin and cookies for fixing the type error.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -49,6 +49,20 @@ const authHandler = NextAuth({
 			}
 		},
 	},
+	session: {
+		jwt: true,
+	},
+	cookies: {
+		sessionToken: {
+			name: `__Secure-next-auth.session-token`,
+			options: {
+				httpOnly: true,
+				sameSite: 'lax',
+				path: '/',
+				secure: process.env.NODE_ENV === 'production',
+			},
+		},
+	},
 });
 
 export { authHandler as GET, authHandler as POST };
